bdde8aa86a17b6ac9893063afaf4d57b96900392
miguel44madera <128770616+miguel44madera@users.noreply.github.com>
2023-03-23 23:53:09 -0400

    Add files via upload

--- Hello World.py
+++ Hello World.py
@@ -1,1 +1,9 @@
-print ("Hello World, my name is Miguel Madera and I love Python!")
+userName = input ("Hello! Please enter your facebook username! ")
+
+userPassword = input ("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "miguel" and userPassword == "CSF534"):
+    print("Welcome to facebook")
+else:
+    print("Wrong credentials, try again")
+
